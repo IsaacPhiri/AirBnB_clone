6231185f3f3ec264646c798ee28df1bd718ad2b6
Isaac Phiri <isaacphiri315@gmail.com>
2022-10-30 16:09:52 +0000

    Edits

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 from uuid import uuid4
 import json
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -12,15 +13,24 @@ class BaseModel:
         """this is a public class attribute"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "__class__":
+                if key == '__class__':
                     continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
                 setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
@@ -29,6 +39,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values
@@ -38,3 +49,23 @@ class BaseModel:
         new_dict["ceated_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
+    
+    def save_json_to_file(self, filename):
+        '''Saves instance of BaseModel to a file'''
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(self.to_dict(), f)
+
+    def save_json_to_file2(self, filename):
+        '''Saves instance of BaseModel to a file'''
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(str(self.to_dict()))
+
+    def load_json_from_file2(filename):
+        '''loads an instance of BaseModel from a file'''
+        with open(filename, "r", encoding="utf-8") as f:
+            return f.read()
+
+    def load_json_from_file(filename):
+        '''loads an instance of BaseModel from a file'''
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
