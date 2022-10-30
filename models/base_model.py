b7ddd0790b5d0ae31fcc4c296a0ca4d0938cc9e9
Isaac Phiri <isaacphiri315@gmail.com>
2022-10-30 15:23:58 +0000

    Edited pycodestyle errors

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,6 @@ from datetime import datetime
 class BaseModel:
     '''Base model class'''
     def __init__(self, *args, **kwargs):
-<<<<<<< HEAD
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-=======
         """this is a public class attribute"""
         if kwargs:
             for key, value in kwargs.items():
@@ -27,17 +22,17 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
->>>>>>> 563bcac19cfaed3b56dc329fc2c1f8240ed494e1
-
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
-    
+
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """ returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["ceated_at"] = self.created_at.isoformat()
